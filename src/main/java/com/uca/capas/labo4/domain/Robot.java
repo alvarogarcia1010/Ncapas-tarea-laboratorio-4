e52cd018d8247d96a6487cf6d9c9e5be889d8a03
alvarogarcia1010 <00111317@uca.edu.sv>
2020-05-07 17:28:19 -0600

    Tarea terminada

---
+++ src/main/java/com/uca/capas/labo4/domain/Robot.java
@@ -0,0 +1,19 @@
+package com.uca.capas.labo4.domain;
+
+import javax.validation.constraints.AssertTrue;
+
+public class Robot {
+	
+	@AssertTrue(message="Ups no puedes agregar tu producto porque eres un robot, haz click para regresar")
+	private Boolean soyRobot;
+
+	public Boolean getSoyRobot() {
+		return soyRobot;
+	}
+
+	public void setSoyRobot(Boolean soyRobot) {
+		this.soyRobot = soyRobot;
+	}
+	
+	
+}
